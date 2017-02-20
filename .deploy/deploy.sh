a8a81d2f688318e912953515ecb1f108b7acb1ac
Luke Woodward <woodward.lucas@gmail.com>
2017-02-20 13:45:08 -0600

    user install instead of update

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-composer update --no-dev --optimize-autoloader
+composer install --no-dev --optimize-autoloader
 rsync -azvh --update --delete-after --quiet --exclude-from $TRAVIS_BUILD_DIR/.deploy/ignore $TRAVIS_BUILD_DIR/ $1@$2:$3
