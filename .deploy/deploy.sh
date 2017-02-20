e2e58a385763fb3268b8f73e9da2022dec32cf67
Luke Woodward <woodward.lucas@gmail.com>
2017-02-20 13:55:47 -0600

    keep rsync log

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
 composer install --no-dev --optimize-autoloader
-rsync -azvh --update --delete-after --quiet --exclude-from $TRAVIS_BUILD_DIR/.deploy/ignore $TRAVIS_BUILD_DIR/ $1@$2:$3
+rsync -azvh --update --delete-after --exclude-from $TRAVIS_BUILD_DIR/.deploy/ignore $TRAVIS_BUILD_DIR/ $1@$2:$3
