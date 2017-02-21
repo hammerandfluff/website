1a2625531bba8c77151f03d819ba9733f582466b
Luke Woodward <woodward.lucas@gmail.com>
2017-02-21 08:08:01 -0600

    remove memcached test code

--- wp-content/themes/hnf/index.php
+++ wp-content/themes/hnf/index.php
@@ -7,17 +7,6 @@
  */
 
 get_header(); ?>
-<?php $key   = 'dummy';
-$value = '100';
-
-$dummy_value = wp_cache_get( $key );
-
-if ( $value !== $dummy_value ) {
-    echo "The dummy value is not in cache. Adding the value now.";
-    wp_cache_set( $key, $value );
-} else {
-    echo "Value is " . $dummy_value . ". The WordPress Memcached Backend is working!";
-}?>
 	<?php if ( have_posts() ) : ?>
 		<?php while ( have_posts() ): the_post(); ?>
 			<h2><?php the_title(); ?></h2>
