d3bbb00ea9c05684237307a9922c77742d96ca5f
Luke Woodward <woodward.lucas@gmail.com>
2017-02-20 22:03:20 -0600

    dump autoload separately

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
-rm -rf vendor
-composer install --no-dev --optimize-autoloader
+composer install --no-dev
+composer dump-autoload -a
 rsync -azvh --update --delete-after --exclude-from $TRAVIS_BUILD_DIR/.deploy/ignore $TRAVIS_BUILD_DIR/ $1@$2:$3
